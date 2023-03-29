411c0b4228f231cce25d056283442db4b5274c24
nadazaki21 <nadazaki019@gmail.com>
2023-03-29 17:49:28 +0200

    google maps done

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyDrfd5svpRqMFPNJ2-FCUbxJKmbG09s9W4";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
+
